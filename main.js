e8b58f0af8e94bd9b419f55c516434c806d35ba0
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-08-27 20:57:31 +0300

    non idea, posted to SO

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ chrome.tabs.onUpdated.addListener(
     if (changeInfo.status === "complete") {
       chrome.tabs.executeScript({
         code: `document.addEventListener('scroll', () => {
-                var allSponsoredPosts = [...document.querySelectorAll('.oajrlxb2.g5ia77u1')]
-                              .filter(x => x.attributes.href && x.attributes.href.textContent.includes('/ads/about'));
-
-                allSponsoredPosts.forEach(x => {
-                  let parent = x;
-                  while (!parent.hasAttribute('data-pagelet')) {
-                    parent = parent.parentElement;
-                  };
-                  parent.remove();
-                })
+                var theAd = document.querySelector('a[href*="/ads/about"]');
+                
+                let parent = theAd;
+                while (!parent.hasAttribute('data-pagelet')) {
+                  parent = parent.parentElement;
+                };
+                parent.remove();
               }, { passive: true });
             `
       });
