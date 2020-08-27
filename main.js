f631c138d28b409b80adbeb88627a76a8fe1f39b
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-08-27 22:57:41 +0300

    new code!!!

--- main.js
+++ main.js
@@ -3,13 +3,23 @@ chrome.tabs.onUpdated.addListener(
     if (changeInfo.status === "complete") {
       chrome.tabs.executeScript({
         code: `document.addEventListener('scroll', () => {
-                var theAd = document.querySelector('a[href*="/ads/about"]');
-                
-                let parent = theAd;
-                while (!parent.hasAttribute('data-pagelet')) {
-                  parent = parent.parentElement;
-                };
-                parent.remove();
+                  var theAds = [...document.querySelectorAll('.qzhwtbm6.knvmm38d .oi732d6d span[id*=jsc]')].filter(x => { 
+                    const cleanWord = x.innerText.replace(/\n/g, '').replace(/\·/g, '').trim();
+            
+                    if(cleanWord.split(' ').length > 1) {
+                        return false;
+                    }
+            
+                    return !(/[0-9]|\s/.test(cleanWord));
+                })
+            
+                theAds.forEach(x => {
+                    let parent = x;
+                    while (!parent.hasAttribute('data-pagelet')) {
+                      parent = parent.parentElement;
+                    };
+                    parent.remove();
+                })  
               }, { passive: true });
             `
       });
